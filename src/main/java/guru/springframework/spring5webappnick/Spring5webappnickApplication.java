f2e8eca87154660a387aba5a4e3015a8b02ddb75
nickolas.grosenia <nickolas@grosenia.co.id>
2020-10-02 18:03:42 +0700

    first commit

---
+++ src/main/java/guru/springframework/spring5webappnick/Spring5webappnickApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webappnick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappnickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappnickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5webappnick/Spring5webappnickApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webappnick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappnickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
