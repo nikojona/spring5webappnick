19512ebab3f5e1f9f6c4ea1feff74f7fcfdb0490
nickolas.grosenia <nickolas@grosenia.co.id>
2020-10-04 08:25:38 +0700

    guru spring framework - 22. Thymeleaf Templates

--- src/main/java/guru/springframework/spring5webappnick/domain/Author.java
+++ src/main/java/guru/springframework/spring5webappnick/domain/Author.java
@@ -67,7 +67,6 @@ public class Author {
 		return "Author [id='" + id + '\'' +
 			   ", firstName='" + firstName + '\'' +  
 			   ", lastName='" + lastName + '\'' +  
-			   ", books='" + books + '\'' +
 			   "]";
 	}
 
--- src/main/java/guru/springframework/spring5webappnick/domain/Book.java
+++ src/main/java/guru/springframework/spring5webappnick/domain/Book.java
@@ -83,7 +83,6 @@ public class Book {
 		return "Book [id='" + id + '\'' + 
 			   ", title='" + title + '\'' +
 			   ", isbn='" + isbn + '\'' +
-			   ", authors='" + authors + '\'' +
 			   "]";
 	}
 
